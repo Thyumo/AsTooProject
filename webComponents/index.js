e32e0fc6977eb9208e8baa42304642e9017d0b7e
Thibault Brazillier <thibault.brazillier@360learning.com>
2024-07-29 09:18:07 +0200

    fix: remove bad export

--- webComponents/index.js
+++ webComponents/index.js
@@ -1,5 +1,4 @@
 export * from "./warningText/main.js";
 export * from "./colorfulText/main.js";
-export * from "./test/main.js";
 export * from "./menubar/main.js";
 export * from "./menubarv2/index.js";
